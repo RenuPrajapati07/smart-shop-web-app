791805ffbef77944d7e64e5e11c4b4ea0891ef93
Renu Prajapati <prajapatirenu790@gmail.com>
2023-05-31 16:38:46 +0530

    corrected contact route path

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Header/>
         <Routes>
           <Route path="/"  element={<Home />} />
-          <Route path="/contact"  element={<Contact />} /> 
+          <Route path="/contacts"  element={<Contact />} /> 
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/reset" element={<Reset />} />
